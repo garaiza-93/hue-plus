103858a6e1c5806b830195a8ed3966c19de2f0fa
garaiza-93 <araizagustavo93@gmail.com>
2022-11-28 23:56:20 -0600

    add devshell

--- flake.nix
+++ flake.nix
@@ -11,5 +11,23 @@
       packages.${system}.default = pkgs.poetry2nix.mkPoetryApplication {
         projectDir = self;
       };
+
+      devShells.${system}.default = pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            poetry
+            portaudio
+            python3Packages.virtualenv # run virtualenv .
+            python3Packages.pyqt5 # avoid installing via pip
+            python3Packages.pyusb # fixes the pyusb 'No backend available' when installed directly via pip
+          ];
+
+          shellHook = with pkgs; ''
+            # fixes libstdc++ issues and libgl.so issues
+              LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib
+              # fixes xcb issues :
+              QT_PLUGIN_PATH=${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}
+          '';
+        };
     };
 }
